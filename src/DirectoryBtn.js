2834d42268aaa2170a233c83351a3ea247f36d5a
Julien Falxa <jfalxa.sta@gmail.com>
2018-10-19 19:22:08 +0200

    feat: add clickable directory button overlay

--- src/DirectoryBtn.js
+++ src/DirectoryBtn.js
@@ -2,21 +2,30 @@ import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 
 export default class DirectoryBtn extends Component {
-  handleLinkClick(event) {
-    event.preventDefault();
-  }
-  render() {
-    return (
-      <button onClick={ this.props.toggle }
-	      className="Btn DirectoryBtn Btn--toggle">
-	{
-	    this.props.isVisible ? (
-		'Hide directory'
-	    ) : (
-		'Show Directory'
-	    )
-	}
-      </button>
-    )
-  }
+    handleLinkClick(event) {
+	event.preventDefault();
+    }
+    render() {
+	return (
+	    <div>
+	      <button onClick={ this.props.toggle }
+		      className="Btn DirectoryBtn Btn--toggle">
+		{
+		    this.props.isVisible ? (
+			'Hide directory'
+		    ) : (
+			'Show Directory'
+		    )
+		}
+	      </button>
+	      {
+		    this.props.isVisible && (
+			    <div
+				  className="Directory--overlay"
+				  onClick={ this.props.toggle }></div>
+		    )
+		}
+	    </div>
+	)
+    }
 }
