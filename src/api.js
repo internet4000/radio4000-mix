d5078f958f24d251a1cb71a6af5c4e56d36ad57f
Julien Falxa <jfalxa.sta@gmail.com>
2018-10-19 19:55:26 +0200

    improve search index

--- src/api.js
+++ src/api.js
@@ -1,26 +1,28 @@
 // serialise object to array of channel objects
 function serialize(channels) {
-  const ids = Object.keys(channels);
-  
-  if(!ids) return
-  
-  return ids.map(key => {
-    let channel = channels[key];
+    const ids = Object.keys(channels);
     
-    channel['id'] = key;
+    if(!ids) return null;
+    
+    return ids.map(key => {
+	let channel = channels[key];
+	
+	channel['id'] = key;
 
-    if(!channel.tracks) {
-      channel.tracks = []
-    } else {
-      channel.tracks = Object.keys(channel.tracks);
-    }
-    
-    return channel
-  })
+	if(!channel.tracks) {
+	    channel.tracks = []
+	} else {
+	    channel.tracks = Object.keys(channel.tracks);
+	}
+
+	channel.searchIndex = `${channel.title || ''} ${channel.description || ''} ${channel.slug || ''}`
+	
+	return channel
+    })
 }
 
 function getChannels() {
-  return fetch('https://radio4000.firebaseio.com/channels.json').then(res => res.json().then(serialize))
+    return fetch('https://radio4000.firebaseio.com/channels.json').then(res => res.json().then(serialize))
 }
 
 export { getChannels };
